129091c7f84e603073d49fe3799d3cf63398a584
matthew <matthewbeaulieu10@gmail.com>
2021-11-29 14:43:40 -0600

    adds another entry field when you press enter

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ function ButtClick(){
 
 function AddMember(event){
     if (event.keyCode == 13) {
-        document.querySelector('#textbox').value='enter was pressed';
+        var input = document.createElement("input");
+        input.setAttribute('id', 'textbox');
+        input.setAttribute('type', 'text');
+        input.setAttribute('onkeyup', 'AddMember(event)');
+        var parent = document.querySelector(".entryfield");
+        parent.appendChild(input);
+        linebreak = document.createElement("br");
+        parent.appendChild(linebreak);
     }
 }
